6f3c66210c150ec5f4ee7779161dc771e7df4be6
abdulaziz0413 <oktava0413@gmail.com>
2024-04-25 12:25:43 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world");
+             Console.Write("Hello world!");
+              Console.WriteLine("Hello world");
+              Console.ReadLine();
+        }
+    }
+    
+}
